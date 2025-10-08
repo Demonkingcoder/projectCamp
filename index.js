c6de84f938b67f3ad847c1a1b5666e9fa3c633cb
Demonkingcoder <129573913+Demonkingcoder@users.noreply.github.com>
2025-10-08 07:46:32 +0530

    changes with .env 2

--- index.js
+++ index.js
@@ -4,10 +4,14 @@ dotenv.config({
     path: "./.env",
 })
 
-let myusername = process.env.username;
-let myusername2 = process.env.username2;
+// let myusername = process.env.username;
+// let myusername2 = process.env.username2;
+let myusername = process.env.database;
+
+// console.log("value: ", myusername);
+// console.log("value: ", myusername2);
 
 console.log("value: ", myusername);
-console.log("value: ", myusername2);
+
 
 console.log("start of backend project");
