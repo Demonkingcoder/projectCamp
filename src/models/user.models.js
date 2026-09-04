7be63dc5ed5265025c53e51831fcc33ab2054b76
Demonkingcoder <anshumankumars04@example.com>
2026-09-04 22:53:37 +0530

    ex

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -76,6 +76,7 @@ userSchema.methods.isPasswordCorrect = async function(password) {
 }
 
 userSchema.methods.generateAccessToken = function(){
+    console.log("ACCESS EXPIRY:", process.env.ACCESS_TOKEN_EXPIRY);
     return jwt.sign(
         {
             _id: this._id,
